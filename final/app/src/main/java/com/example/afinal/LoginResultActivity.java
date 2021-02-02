3d63ddd73b0ce17749821b6a78edf0999c3e3e94
leeseunghun98 <leeseunghun98@naver.com>
2021-02-03 01:16:41 +0900

    Original Login Project

---
+++ final/app/src/main/java/com/example/afinal/LoginResultActivity.java
@@ -0,0 +1,26 @@
+package com.example.afinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginResultActivity extends AppCompatActivity {
+    TextView TextView_get;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView_get = findViewById(R.id.TextView_get);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String email =  bundle.getString("email");
+        String password = bundle.getString("password");
+
+        TextView_get.setText(email + "/" + password);
+    }
+}
